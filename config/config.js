3c84c5c5125518ebbf2c802af152143ebae755fb
oze4 <matthewpoestreich@gmail.com>
2019-05-05 15:31:07 -0500

    .gitignore Fixed

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    db: {
-        connectionString: "mongodb://ostrike:theempireofthelights@mongo.ostrike.com:27017",
-        authenticationDatabase: "admin",
-    },
-    secret: "8cde6d63-898e-4901-ab63-81a1859ae846-5ae69741-733f-47f6-ad3c-32b548b6dd95-84bd6107-6ad4-4b21-a8a1-a6710cd65bfb",
-    port: 9000,
-}
