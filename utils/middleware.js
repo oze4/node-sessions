999e79f6459a0bd364278aead278312e1d29d223
Matt Oestreich <matthewpoestreich@gmail.com>
2019-05-05 22:03:42 -0500

    Update middleware.js

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,9 +1,6 @@
 module.exports = {
-
-
-    // middleware function to check for logged-in users
+    
     sessionChecker: function (req, res, next) {
-        console.log("\r\n\r\nSESSION_CHECKER\r\n");
         try {
             if (req.session.user && req.cookies.user_sid) {
                 res.redirect('/dashboard');
@@ -15,10 +12,7 @@ module.exports = {
         }
     },
 
-    // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-    // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
     cookieChecker: function (req, res, next) {
-        console.log("\r\n\r\nCHECKED COOKIE!\r\n\r\n")
         try { 
             if (req.cookies.user_sid && !req.session.user) {
                 res.clearCookie('user_sid');
@@ -31,7 +25,6 @@ module.exports = {
 
     routeNotFound: function (req, res, next) {
         res.status(404).send("Sorry can't find that!")
-    }
-
+    },
 
 }
