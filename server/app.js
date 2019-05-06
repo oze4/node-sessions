3138a0f1fc1b305abcf127a5af0bb29af0ab41ec
Matt Oestreich <matthewpoestreich@gmail.com>
2019-05-05 21:15:22 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,14 +15,18 @@ const HomeController = require('../controllers/home.controller.js');
 // set app port
 app.set('port', config.port);
 
+
 // morgan for logging
 app.use(morgan('dev'));
+
 
 // initialize body-parser to parse incoming parameters requests to req.body
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // initialize cookie-parser to allow us access the cookies stored in the browser. 
 app.use(cookieParser());
+
 
 // initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
@@ -35,42 +39,18 @@ app.use(session({
     }
 }));
 
-// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+
+// cookie checker app middleware
 app.use(middleware.cookieChecker);
-/*app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');
-    }
-    next();
-});
 
-// middleware function to check for logged-in users
-const sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.redirect('/dashboard');
-    } else {
-        next();
-    }
-};
-
-// route for Home-Page
-app.get('/', sessionChecker, (req, res) => {
-    res.redirect('/user/login');
-});
-*/
 
 // set up main routes
 app.use('/', HomeController)
 app.use('/', UserController);
 
 
-// route for handling 404 requests(unavailable routes)
-//app.use(middleware.routeNotFound);
-app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!")
-});
-
+// route not found middleware
+app.use(middleware.routeNotFound);
 
 
 module.exports = app;
