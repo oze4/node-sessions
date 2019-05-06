30764b9d57601bde8d1f096aff9919b1cf23a481
oze4 <matthewpoestreich@gmail.com>
2019-05-05 22:33:12 -0500

    Update home.controller.js

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,14 +4,19 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const middleware = require('../utils/middleware.js');
 
-
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(cookieParser());
 
 
+
+
+// GET REQUESTS ----------------------------------------------
 router.get('/', middleware.sessionChecker, (req, res) => {
     res.redirect('/login');
 });
+//end GET REQUESTS -------------------------------------------
+
+
 
 
 module.exports = router;
