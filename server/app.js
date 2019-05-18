bd7d055fae7a6732e05edb351b12a9563de4bfce
Matt Oestreich <matthewpoestreich@gmail.com>
2019-05-17 22:48:38 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,58 +1,43 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-// utils
-const config = require('../utils/config.js');
-const middleware = require('../utils/middleware.js');
-const session = require('../utils/session.js');
-
-// controllers/routes
+const express        = require('express');
+const app            = express();
+const morgan         = require('morgan');
+const bodyParser     = require('body-parser');
+const cookieParser   = require('cookie-parser');
+const config         = require('../utils/config.js');
+const middleware     = require('../utils/middleware.js');
+const session        = require('../utils/session.js');
 const HomeController = require('../controllers/home.controller.js');
 const UserController = require('../controllers/user.controller.js');
-
 
 
 
 // set view engine for templating
 app.set('view engine', 'hbs')
 
-
 // set app port
 app.set('port', config.port);
-
 
 // morgan for logging
 app.use(morgan('dev'));
 
-
 // allows us to easily parse the request body
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 // allow us to easily parse session cookies
 app.use(cookieParser());
 
-
 // allows us to track logged in user across sessions
 app.use(session.sessionConfig);
 
-
 // helps keep session cookie clean
 app.use(middleware.cookieChecker);
-
 
 // set up main routes
 app.use('/', HomeController)
 app.use('/', UserController);
 
-
 // 404 middleware
 app.use(middleware.routeNotFound);
-
-
 
 
 module.exports = app;
